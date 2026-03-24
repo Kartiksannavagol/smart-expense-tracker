0df50b983b9a1060369c73701b842f094ad9db07
Kartik Sannavagol <kartiksannavagol@gmail.com>
2026-03-25 00:07:33 +0530

    Connected frontend to deployed backend

--- frontend/src/api/expenses.js
+++ frontend/src/api/expenses.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://127.0.0.1:8000";
+const API = "https://smart-expense-backend-7zgw.onrender.com";
 
 export const getExpenses = async () => {
   const res = await axios.get(`${API}/expenses`);
